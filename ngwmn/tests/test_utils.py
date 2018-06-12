96075e28c61712ded757201e1710dda07c16c55b
Andrew Yan <ayan@usgs.gov>
2018-06-12 11:53:54 -0500

    add some simple tests

---
+++ ngwmn/tests/test_utils.py
@@ -0,0 +1,40 @@
+"""
+Unit tests for utility functions
+"""
+
+from unittest import TestCase, mock
+
+import requests as r
+
+from ..utils import get_well_lithography
+
+
+class TestGetWellLithography(TestCase):
+
+    def setUp(self):
+        self.test_service_root = 'http://fake.gov'
+        self.test_agency_cd = 'DOOP'
+        self.test_location_id = 'BP-1729'
+        self.test_xml = '<site><agency>DOOP</agency><id>BP-1729</id></site>'
+
+    @mock.patch('ngwmn.utils.r.get')
+    def test_success(self, r_mock):
+        m_resp = mock.Mock(r.Response)
+        m_resp.content = self.test_xml
+        m_resp.status_code = 200,
+        r_mock.return_value = m_resp
+        result = get_well_lithography(self.test_service_root, self.test_agency_cd, self.test_location_id)
+        self.assertEqual(result.tag, 'site')
+        r_mock.assert_called_with(
+            'http://fake.gov/ngwmn/iddata',
+            params={'request': 'well_log', 'agency_cd': 'DOOP', 'siteNo': 'BP-1729'}
+        )
+
+    @mock.patch('ngwmn.utils.r.get')
+    def test_syntax_error(self, r_mock):
+        m_resp = mock.Mock(r.Response)
+        m_resp.content = 'Stuff'
+        m_resp.status_code = 200,
+        r_mock.return_value = m_resp
+        result = get_well_lithography(self.test_service_root, self.test_agency_cd, self.test_location_id)
+        self.assertIsNone(result)
--- ngwmn/views.py
+++ ngwmn/views.py
@@ -3,7 +3,7 @@ NGWMN UI application views
 """
 from urllib.parse import urljoin
 
-from flask import render_template
+from flask import abort, render_template
 import requests as r
 
 from . import app
@@ -16,29 +16,32 @@ SERVICE_ROOT = app.config.get('SERVICE_ROOT')
 @app.route('/well-location/<agency_cd>/<location_id>', methods=['GET'])
 def well_page(agency_cd, location_id):
     root = get_well_lithography(SERVICE_ROOT, agency_cd, location_id)
-    geolocation_element = root.findall('.//gml:Point/gml:pos', namespaces=root.nsmap)[0]
-    geolocation = geolocation_element.text
-    latitude, longitude = geolocation.split(' ')
-    lat_lower = float(latitude) - 0.01
-    lon_lower = float(longitude) - 0.01
-    lat_upper = float(latitude) + 0.01
-    lon_upper = float(longitude) + 0.01
-    data = {
-        'SERVICE': 'WFS',
-        'VERSION': '1.0.0',
-        'srsName': 'EPSG:4326',
-        'outputFormat': 'GML2',
-        'typeName': 'ngwmn:VW_GWDP_GEOSERVER',
-        'CQL_FILTER': "((QW_SN_FLAG='1') OR (WL_SN_FLAG='1')) AND (BBOX(GEOM,{},{},{},{}))".format(
-            lon_lower,
-            lat_lower,
-            lon_upper,
-            lat_upper
-        )
-    }
-    params = {'request': 'GetFeature'}
-    target = urljoin(SERVICE_ROOT, 'ngwmn/geoserver/wfs')
-    resp = r.post(target, params=params, data=data)
-    summary = parse_xml(resp.content)
-    location_name = summary.find('.//ngwmn:SITE_NAME', namespaces=summary.nsmap)
-    return render_template('well_location.html', location_name=location_name.text)
+    if root is not None and 'gml' in root.nsmap.keys():
+        geolocation_element = root.find('.//gml:Point/gml:pos', namespaces=root.nsmap)
+        geolocation = geolocation_element.text
+        latitude, longitude = geolocation.split(' ')
+        lat_lower = float(latitude) - 0.01
+        lon_lower = float(longitude) - 0.01
+        lat_upper = float(latitude) + 0.01
+        lon_upper = float(longitude) + 0.01
+        data = {
+            'SERVICE': 'WFS',
+            'VERSION': '1.0.0',
+            'srsName': 'EPSG:4326',
+            'outputFormat': 'GML2',
+            'typeName': 'ngwmn:VW_GWDP_GEOSERVER',
+            'CQL_FILTER': "((QW_SN_FLAG='1') OR (WL_SN_FLAG='1')) AND (BBOX(GEOM,{},{},{},{}))".format(
+                lon_lower,
+                lat_lower,
+                lon_upper,
+                lat_upper
+            )
+        }
+        params = {'request': 'GetFeature'}
+        target = urljoin(SERVICE_ROOT, 'ngwmn/geoserver/wfs')
+        resp = r.post(target, params=params, data=data)
+        summary = parse_xml(resp.content)
+        location_name = summary.find('.//ngwmn:SITE_NAME', namespaces=summary.nsmap).text
+        return render_template('well_location.html', location_name=location_name)
+    else:
+        return abort(404)
