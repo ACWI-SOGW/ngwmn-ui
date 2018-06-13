ae8779f0d04365829ea3ea456e712fc46bb30663
Andrew Yan <ayan@usgs.gov>
2018-06-13 10:01:23 -0500

    add simple error handling for request errors

--- ngwmn/tests/test_views.py
+++ ngwmn/tests/test_views.py
@@ -30,11 +30,39 @@ class TestWellPageView(TestCase):
         well_lith_mock.return_value = etree.fromstring(self.test_well_xml)
         post_resp = mock.Mock(r.Response)
         post_resp.content = self.test_summary_xml
+        post_resp.status_code = 200
         post_mock.return_value = post_resp
 
         response = self.app_client.get('/well-location/{}/{}'.format(self.test_agency_cd, self.test_location_id))
         post_mock.assert_called_once()
         self.assertEqual(response.status_code, 200)
+
+    @mock.patch('ngwmn.views.r.post')
+    @mock.patch('ngwmn.views.get_well_lithography')
+    def test_failed_service_with_non_server_error(self, well_lith_mock, post_mock):
+        well_lith_mock.return_value = etree.fromstring(self.test_well_xml)
+        post_resp = mock.Mock(r.Response)
+        post_resp.content = self.test_summary_xml
+        post_resp.status_code = 403
+        post_resp.reason = 'Forbidden'
+        post_mock.return_value = post_resp
+
+        response = self.app_client.get('/well-location/{}/{}'.format(self.test_agency_cd, self.test_location_id))
+        post_mock.assert_called_once()
+        self.assertEqual(response.status_code, 200)
+
+    @mock.patch('ngwmn.views.r.post')
+    @mock.patch('ngwmn.views.get_well_lithography')
+    def test_failed_service_with_server_error(self, well_lith_mock, post_mock):
+        well_lith_mock.return_value = etree.fromstring(self.test_well_xml)
+        post_resp = mock.Mock(r.Response)
+        post_resp.content = self.test_summary_xml
+        post_resp.status_code = 500
+        post_mock.return_value = post_resp
+
+        response = self.app_client.get('/well-location/{}/{}'.format(self.test_agency_cd, self.test_location_id))
+        post_mock.assert_called_once()
+        self.assertEqual(response.status_code, 503)
 
     @mock.patch('ngwmn.views.get_well_lithography')
     def test_no_xml(self, well_lith_mock):
