fcda5ba78f8a9a59461b4c9419e2e572fb2a37da
Daniel Naab <dnaab@usgs.gov>
2018-07-24 16:32:19 -0500

    Add home page with links to sample sites, for test/dev purposes.

--- server/ngwmn/views.py
+++ server/ngwmn/views.py
@@ -3,11 +3,41 @@ NGWMN UI application views
 
 """
 
-from flask import abort, render_template
+from flask import abort, jsonify, render_template
 
-from . import app
+from . import __version__, app
 from .services.ngwmn import get_features, get_iddata, get_water_quality_activities
 from .services.sifta import get_cooperators
+
+
+@app.route('/')
+def home():
+    return render_template(
+        'index.html',
+        version=__version__,
+        test_sites=[
+            {'agency_cd': 'USGS', 'location_id': '353945105574502'},
+            {'agency_cd': 'USGS', 'location_id': '282532081075601'},
+            {'agency_cd': 'USGS', 'location_id': '473442118162201'},
+            {'agency_cd': 'USGS', 'location_id': '423532088254601'},
+            {'agency_cd': 'USGS', 'location_id': '401105074120205'},
+            {'agency_cd': 'USGS', 'location_id': '411958079540202'},
+            {'agency_cd': 'ADWR', 'location_id': '334306112433801'},
+            {'agency_cd': 'DGS', 'location_id': 'Eb53-33'},
+            {'agency_cd': 'KSGS', 'location_id': '381107098532401'},
+            {'agency_cd': 'MEGS', 'location_id': '39412'},
+            {'agency_cd': 'MBMG', 'location_id': '235474'},
+            {'agency_cd': 'TWRB', 'location_id': '2763901'}
+        ]
+    )
+
+
+@app.route('/version')
+def version():
+    """Render the home page."""
+    return jsonify({
+        'version': __version__
+    })
 
 
 @app.route('/site-location/<agency_cd>/<location_id>/', methods=['GET'])
