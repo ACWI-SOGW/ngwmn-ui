d88d8132e54da632f198ad926f12f194333dd185
Mary Bucknell <mbucknell@usgs.gov>
2018-10-30 10:46:57 -0500

    First cut at writing the rendering function for the water levels table.

--- assets/src/scripts/components/water-level-table/index.js
+++ assets/src/scripts/components/water-level-table/index.js
@@ -6,14 +6,48 @@ import { getWaterLevels } from 'ngwmn/services/state/index';
 
 import { isTableRendered } from 'state';
 
-const HEADINGS = [
-    ''
-]
+const COLUMN_HEADINGS = [
+    'Sample Time',
+    'Original Value',
+    'Original Unit',
+    'Accuracy Value',
+    'Accuracy Unit',
+    'Data Provided by',
+    'Depth to water, feet below land surface',
+    'Water level in feet relative to NAVD88'
+];
 
 const drawTable = function(node, waterLevels, table) {
     table = table || node.append('table')
         .classed('usa-table', true);
+    const tableData = waterLevels.map((sample) => {
+        return [
+            sample.time,
+            sample.originalValue,
+            sample.unit,
+            sample.accuracyValue,
+            sample.accuracyUnit,
+            sample.sourceCode,
+            sample.fromLandsurfaceValue,
+            sample.fromDatumValue
+        ];
+    });
+    table.append('thead')
+        .append('tr')
+            .selectAll('th')
+            .data(COLUMN_HEADINGS).enter()
+            .append('th')
+                .text((col) => col);
 
+    const rows = table.append('tbody')
+        .selectAll('tr')
+        .data(tableData).enter()
+        .append('tr');
+
+    rows.selectAll('td')
+        .data((row) => row)
+        .append('td')
+            .text((d) => d);
 
     return table;
 }
