cf36c8b1537efacff98b9852e3e5b2553eb9d587
Aaron Briggs <abriggs@contractor.usgs.gov>
2018-12-14 09:22:44 -0600

    slimmed down the logic for subtile creation

--- server/ngwmn/string_utils.py
+++ server/ngwmn/string_utils.py
@@ -6,7 +6,7 @@ utility functions for the manipulation of strings
 
 def generate_subtitle(features):
     """
-    logic to create subtitle -- which could also be called the 'monitoring location description'
+    logic to create subtitle (the 'monitoring location description')
     :param features: python dictionary with features of the selected monitoring location
     :return subtitle: the final subtitle string ready for display
     """
@@ -15,74 +15,23 @@ def generate_subtitle(features):
     county = features.get('COUNTY_NM')
     state = features.get('STATE_NM')
 
-    intro_clause = ''
-    main_clause = ''
-    closing_period = ''
-
-    if site_type or aquifer_description:
-        only_state_in_features = False
-
-        if state:
-            intro_clause = generate_intro_clause(state, county, only_state_in_features)
-        main_clause = generate_main_clause(state, site_type, aquifer_description)
-
-    elif state:
-        only_state_in_features = True
-        intro_clause = generate_intro_clause(state, county, only_state_in_features)
-
-    if site_type or aquifer_description or state:
-        closing_period = '.'
-
-    subtitle = '{0}{1}{2}'.format(intro_clause, main_clause, closing_period)
-    return subtitle
-
-
-def generate_intro_clause(state, county, only_state_in_features):
-    """
-    Creates the section of the monitoring location description that denotes the state and possibly county
-    :param state: string, name of the state where the monitoring location is found
-    :param county: string, name of the county if available
-    :param only_state_in_features: boolean, if the name of state
-    :return state_clause: string, section of the monitoring location description that lists the state and possibly
-            county names
-    """
-    if only_state_in_features:
-        clause_start = 'This groundwater monitoring location is in '
-        clause_end = county + ', ' + state if county else state
-
+    # In some records the state name is blank, so check that the state name is there (also check if the county name
+    # is there. If not, adjust for that)
+    if state:
+        start_of_description = 'Located in {0}, {1}, this '.format(county, state) if county\
+            else 'Located in {0}, this '.format(state)
+    # If the state name is blank, start the sentence in a sensible manner
     else:
-        clause_start = 'Located in ' + county + ', ' + state if county else 'Located in ' + state
-        clause_end = ', this '
+        start_of_description = 'This '
 
-    state_clause = '{0}{1}'.format(clause_start, clause_end)
+    end_of_description = 'groundwater monitoring location is associated with a '
+    # Check if the site type is 'WELL' if it is, add the word water, else don't
+    end_of_description = '{0}water {1}'.format(end_of_description, site_type.lower()) if site_type == 'WELL' \
+        else '{0}{1}'.format(end_of_description, site_type.lower())
 
-    return state_clause
+    # Add the Nation Aquifer description to end of the description
+    end_of_description = '{0} in the {1}'.format(end_of_description, aquifer_description)
 
+    subtitle = '{0}{1}.'.format(start_of_description, end_of_description)
 
-def generate_main_clause(state, site_type, aquifer_description):
-    """
-    Creates the section of the monitoring location description denoting the site type and aquifer
-    :param state: string, name of the state where the monitoring location is found, used as a boolean test
-    :param site_type: string, type of location, usually a well or spring
-    :param aquifer_description: string, the name of the aquifer in which the monitoring location is found
-    :return site_type_clause: string, the section of the monitoring location description denoting
-            the site type and aquifer
-    """
-    site_type_clause = ''
-
-    if not state:
-        site_type_clause = 'This '
-
-    if site_type:
-        site_type_clause = '{0}groundwater monitoring location is associated with a '.format(site_type_clause)
-        site_type_clause = \
-            '{0}water {1}'.format(site_type_clause, site_type.lower()) if site_type == 'WELL' else '{0}{1}'\
-            .format(site_type_clause, site_type.lower())
-
-        if aquifer_description:
-            site_type_clause = '{0} in the {1}'.format(site_type_clause, aquifer_description)
-
-    elif aquifer_description:
-        site_type_clause = '{0}groundwater monitoring location is in the {1}'.format(site_type_clause,
-                                                                                     aquifer_description)
-    return site_type_clause
+    return subtitle
--- server/ngwmn/tests/test_string_utils.py
+++ server/ngwmn/tests/test_string_utils.py
@@ -4,129 +4,46 @@ Unit tests for string manipulation functions
 
 from unittest import TestCase
 
-from ..string_utils import generate_subtitle, generate_intro_clause, generate_main_clause
+from ..string_utils import generate_subtitle
 
 
 class TestGenerateSubtitle(TestCase):
     def setUp(self):
-        self.features_none = {}
-        self.features_county = {'COUNTY_NM': 'Dane County'}
-        self.features_state = {'STATE_NM': 'Wisconsin'}
-        self.features_aquifer = {'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
-        self.features_site_type_well = {'SITE_TYPE': 'WELL'}
-        self.features_site_type_not_well = {'SITE_TYPE': 'something not a well'}
-        self.features_state_n_site = {'STATE_NM': 'Wisconsin', 'SITE_TYPE': 'WELL'}
-        self.features_state_county_n_site = {'STATE_NM': 'Wisconsin', 'COUNTY_NM': 'Dane County', 'SITE_TYPE': 'WELL'}
-        self.features_all = {'STATE_NM': 'Wisconsin', 'COUNTY_NM': 'Dane County', 'SITE_TYPE': 'WELL',
+        self.features_all = {'STATE_NM': 'Wisconsin',
+                             'COUNTY_NM': 'Dane County',
+                             'SITE_TYPE': 'WELL',
                              'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
-        self.features_state_county_aquifer = {'STATE_NM': 'Wisconsin', 'COUNTY_NM': 'Dane County',
-                                              'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
-        self.features_site_n_aquifer = {'SITE_TYPE': 'WELL', 'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
+        self.features_site_type_not_well = {'STATE_NM': 'Wisconsin',
+                                            'COUNTY_NM': 'Dane County',
+                                            'SITE_TYPE': 'something that is not a well',
+                                            'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
+        self.features_no_state = {'COUNTY_NM': 'Dane County',
+                                  'SITE_TYPE': 'WELL',
+                                  'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
+        self.feature_no_county = {'STATE_NM': 'Wisconsin',
+                                  'SITE_TYPE': 'WELL',
+                                  'NAT_AQFR_DESC': 'cambrian ordovician aquifer system'}
 
-    def test_no_features(self):
-        result = generate_subtitle(self.features_none)
-        self.assertEqual('', result, 'Expected the results to be blank when no features are present')
-
-    def test_only_county(self):
-        result = generate_subtitle(self.features_county)
-        self.assertEqual('', result, 'Expected the results to be blank when only the county is in features')
-
-    def test_only_state(self):
-        result = generate_subtitle(self.features_state)
-        self.assertEqual('This groundwater monitoring location is in Wisconsin.', result,
-                         'Result not as expected when only state in features')
-
-    def test_only_aquifer(self):
-        result = generate_subtitle(self.features_aquifer)
-        self.assertEqual('This groundwater monitoring location is in the cambrian ordovician aquifer system.', result,
-                         'Result not as expected when only aquifer is in features')
-
-    def test_site_type_well(self):
-        result = generate_subtitle(self.features_site_type_well)
-        self.assertEqual('This groundwater monitoring location is associated with a water well.',
-                         result, 'Result not as expected when site type is well')
-
-    def test_site_type_not_well(self):
-        result = generate_subtitle(self.features_site_type_not_well)
-        self.assertEqual('This groundwater monitoring location is associated with a something not a well.',
-                         result, 'Result not as expected when site type is something other than a well')
-
-    def test_site_type_n_state(self):
-        result = generate_subtitle(self.features_state_n_site)
-        self.assertEqual('Located in Wisconsin, this groundwater monitoring location is associated with a water well.',
-                         result, 'Result not as expected when site type and state are in features.')
-
-    def test_site_state_n_county(self):
-        result = generate_subtitle(self.features_state_county_n_site)
-        self.assertEqual('Located in Dane County, Wisconsin, this groundwater monitoring location is associated'
-                         ' with a water well.',
-                         result, 'Result not as expected when site, state, and county are in features.')
-
-    def test_state_county_site_n_aquifer(self):
+    def test_all_features_present(self):
         result = generate_subtitle(self.features_all)
         self.assertEqual('Located in Dane County, Wisconsin, this groundwater monitoring location is associated with '
                          'a water well in the cambrian ordovician aquifer system.',
                          result, 'Result not as expected when site, state, county, and aquifer are in features.')
 
-    def test_state_county_n_aquifer(self):
-        result = generate_subtitle(self.features_state_county_aquifer)
-        self.assertEqual('Located in Dane County, Wisconsin, this groundwater monitoring location is in the '
-                         'cambrian ordovician aquifer system.',
-                         result, 'Result not as expected when site, state, and aquifer are in features.')
-
-    def test_site_n_aquifer(self):
-        result = generate_subtitle(self.features_site_n_aquifer)
-        self.assertEqual('This groundwater monitoring location is associated with a water well in the'
-                         ' cambrian ordovician aquifer system.',
-                         result, 'Result not as expected when only site and aquifer are in features.')
-
-
-class TestGenerateIntroClause(TestCase):
-    def setUp(self):
-        self.state_name = 'Wisconsin'
-        self.county_name = 'Dane County'
-        self.only_state_in_features_false = False
-        self.only_state_in_features_true = True
-
-    def test_state_in_features_true(self):
-        result = generate_intro_clause(self.state_name, self.county_name, self.only_state_in_features_true)
-        self.assertEqual('This groundwater monitoring location is in Dane County, Wisconsin',
-                         result, 'Result not as expected when only_state_in_features_true.')
-
-    def test_state_in_features_false(self):
-        result = generate_intro_clause(self.state_name, self.county_name, self.only_state_in_features_false)
-        self.assertEqual('Located in Dane County, Wisconsin, this ',
-                         result, 'Result not as expected when only_state_in_features_false.')
-
-
-class TestGenerateMainClause(TestCase):
-    def setUp(self):
-        self.state_name_present = True
-        self.site_type_well = 'WELL'
-        self.site_type_not_well = 'something not a well'
-        self.aquifer_description = 'cambrian ordovician aquifer system'
-        self.aquifer_description_none = ''
-
-    def test_site_type_well(self):
-        result = generate_main_clause(self.state_name_present, self.site_type_well, self.aquifer_description)
-        self.assertEqual('groundwater monitoring location is associated with a water well in the'
-                         ' cambrian ordovician aquifer system',
-                         result, 'Result not expected when site_type is well')
-
     def test_site_type_not_well(self):
-        result = generate_main_clause(self.state_name_present, self.site_type_not_well, self.aquifer_description)
-        self.assertEqual('groundwater monitoring location is associated with a something not a well in the'
-                         ' cambrian ordovician aquifer system',
-                         result, 'Result not expected when site_type is NOT well')
+        result = generate_subtitle(self.features_site_type_not_well)
+        self.assertEqual('Located in Dane County, Wisconsin, this groundwater monitoring location is associated with a'
+                         ' something that is not a well in the cambrian ordovician aquifer system.',
+                         result, 'Result not as expected when site type is something other than a well')
 
-    def test_state_name_not_present(self):
-        self.state_name_present = False
-        result = generate_main_clause(self.state_name_present, self.site_type_not_well, self.aquifer_description)
-        self.assertEqual('This groundwater monitoring location is associated with a something not a well in the'
-                         ' cambrian ordovician aquifer system',
-                         result, 'Result not expected when state_name NOT present')
+    def test_no_state_in_features(self):
+        result = generate_subtitle(self.features_no_state)
+        self.assertEqual('This groundwater monitoring location is associated with a water well in'
+                         ' the cambrian ordovician aquifer system.',
+                         result, 'Result not as expected when state name NOT in features.')
 
-    def test_aquifer_description_none(self):
-        result = generate_main_clause(self.state_name_present, self.site_type_not_well, self.aquifer_description_none)
-        self.assertEqual('groundwater monitoring location is associated with a something not a well',
-                         result, 'Result not expected when aquifer_description NOT present')
+    def test_no_county_in_features(self):
+        result = generate_subtitle(self.feature_no_county)
+        self.assertEqual('Located in Wisconsin, this groundwater monitoring location is associated with a water well'
+                         ' in the cambrian ordovician aquifer system.',
+                         result, 'Result not as expected when county name NOT in features')
