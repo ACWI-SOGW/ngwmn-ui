28b0387fba6caf0353f606038b96eb0c99d21933
Aaron Briggs <abriggs@contractor.usgs.gov>
2019-02-22 14:03:07 -0600

    moved a space to please Codacy

--- server/ngwmn/views.py
+++ server/ngwmn/views.py
@@ -63,14 +63,12 @@ def provider(agency_cd):
     if agency_cd not in providers_by_agency_cd:
         return '{0} is not a valid agency code'.format(agency_cd), 404
 
-    return render_template('provider.html',
-                           agency_metadata=providers_by_agency_cd.get(agency_cd),
+    return render_template('provider.html', agency_metadata=providers_by_agency_cd.get(agency_cd),
                            provider_content=pull_feed(confluence_url(agency_cd, MAIN_CONTENT)),
                            site_selection=pull_feed(confluence_url(agency_cd, SITE_SELECTION_CONTENT)),
                            data_collection=pull_feed(confluence_url(agency_cd, DATA_COLLECTION_CONTENT)),
                            data_management=pull_feed(confluence_url(agency_cd, DATA_MANAGEMENT_CONTENT)),
-                           other_agency_info=pull_feed(confluence_url(agency_cd, OTHER_AGENCY_INFO_CONTENT))
-                           )
+                           other_agency_info=pull_feed(confluence_url(agency_cd, OTHER_AGENCY_INFO_CONTENT)))
 
 
 @app.route('/provider/<agency_cd>/site/', methods=['GET'])
