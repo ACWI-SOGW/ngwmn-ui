590c1ec5418a3891ca60372437cbbb4a3dc498c8
Aaron Briggs <abriggs@contractor.usgs.gov>
2020-12-01 13:09:56 -0600

    clean up

--- server/config.py
+++ server/config.py
@@ -15,6 +15,3 @@ STATISTICS_METHODS_URL = CONFLUENCE_URL + 'createrssfeed.action?types=page&space
 
 # URL pattern for retrieving SIFTA cooperator logos
 COOPERATOR_SERVICE_PATTERN = 'https://water.usgs.gov/customer/stories/{site_no}&StartDate=10/1/{year}&EndDate={current_date}'
-
-COOPERATOR_SERVICE_PATTERN_OLD = 'https://sifta.water.usgs.gov/Services/REST/Site/CustomerFunding.ashx?' \
-                             'SiteNumber={site_no}&StartDate=10/1/{year}&EndDate={current_date}'
