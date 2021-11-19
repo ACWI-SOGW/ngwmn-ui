a1deb3219211172e5d45233a59be8f3354200cef
David Uselmann <duselmann@usgs.gov>
2021-11-19 15:42:59 -0600

    first broad stroke pass at remote content

--- server/config.py
+++ server/config.py
@@ -7,11 +7,10 @@ DEBUG = False
 SERVICE_ROOT = 'https://cida.usgs.gov'
 STATS_SERVICE_ROOT = 'http://cida.usgs.gov/ngwmn_statistics'
 
-CONFLUENCE_URL = 'https://my.usgs.gov/confluence/'
+# TODO this will be revised to be loaded from configuration
+CONFLUENCE_URL = 'http://ngwmn-dev.cr.usgs.gov:8080/ngwmn-remote-content/content/'
 
-STATISTICS_METHODS_URL = CONFLUENCE_URL + 'createrssfeed.action?types=page&spaces=GWDataPortal&title' \
-                         '=NGWMN+Statistics+Methods&labelString=ngwmn-stat-meth&excludedSpaceKeys%3D&sort=modified' \
-                         '&maxResults=10&timeSpan=5000&showContent=true&confirm=Create+RSS+Feed '
+STATISTICS_METHODS_URL = CONFLUENCE_URL + 'ngwmn-stat-meth'
 
 # URL pattern for retrieving SIFTA cooperator logos
 COOPERATOR_SERVICE_PATTERN = 'https://water.usgs.gov/customer/stories/{site_no}&StartDate=10/1/{year}&EndDate={current_date}'
