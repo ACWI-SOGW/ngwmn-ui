c82a27dd386d8728228594f43807a0851a366cc9
David Uselmann <duselmann@usgs.gov>
2021-12-06 16:46:42 -0600

    remove unused configuration variable

--- server/config.py
+++ server/config.py
@@ -9,6 +9,5 @@ DEBUG = False
 SERVICE_ROOT = 'https://cida.usgs.gov'
 STATS_SERVICE_ROOT = 'http://cida.usgs.gov/ngwmn_statistics'
 CONFLUENCE_URL = 'place holder - see dev tier for workspace value'
-STATISTICS_METHODS_URL = 'place holder - see dev tier for workspace value'
 # URL pattern for retrieving SIFTA cooperator logos
 COOPERATOR_SERVICE_PATTERN = 'https://water.usgs.gov/customer/stories/{site_no}&StartDate=10/1/{year}&EndDate={current_date}'
